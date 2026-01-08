03b1d313c5cdb6432549ede6b9d92d339b183552
AbinayaThapaChhetri <abhinayathapa164@gmail.com>
2026-01-08 14:37:09 +0545

    Add files via upload

---
+++ A_Binary_Array_Game.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,c=0,b=0,m=0;
+        cin>>n;
+
+     vector<int>a(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        
+            
+            
+            
+            }
+            if(a[0]==1 ||a[n-1]==1)
+        {
+            cout<<"Alice"<<endl;
+        }
+        else
+        {
+            cout<<"Bob"<<endl;
+        }
+
+            
+
+
+        }
+        
+    
+}
