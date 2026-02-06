15fc3db6c56953eff66d1324a8f8856d36e7b467
AbinayaThapaChhetri <abhinayathapa164@gmail.com>
2026-02-06 20:29:32 +0545

    Add files via upload

---
+++ A_Cinema_Line.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int c25 = 0, c50 = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] == 25)
+        {
+            c25++;
+        }
+        else if(a[i] == 50)
+        {
+            if(c25 == 0)
+            {
+                cout << "NO";
+                return 0;
+            }
+            c25--;
+            c50++;
+        }
+        else
+        {
+            if(c50 > 0 && c25 > 0)
+            {
+                c50--;
+                c25--;
+            }
+            else if(c25 >= 3)
+            {
+                c25 -= 3;
+            }
+            else
+            {
+                cout << "NO";
+                return 0;
+            }
+        }
+    }
+
+    cout << "YES";
+}
