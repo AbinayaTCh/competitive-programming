4385cba9e29e0f40221ac85a64709181369d4824
AbinayaThapaChhetri <abhinayathapa164@gmail.com>
2026-02-16 20:24:20 +0545

    Add files via upload

---
+++ B_New_Year_s_Number.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long n,c=1;
+        cin>>n;
+        if(n/2020>=n%2020)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+}
+}
