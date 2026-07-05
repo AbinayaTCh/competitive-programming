ecb2da9a2ab488ecfba110750bc5bc891c5fe2d0
AbinayaThapaChhetri <abhinayathapa164@gmail.com>
2026-07-05 13:10:12 +0545

    Add files via upload

---
+++ Supermajority.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ll a,b;
+cin>>a>>b;
+if(3*a>2*b)
+{
+    cout<<"Yes"<<endl;
+}
+else
+{
+    cout<<"No"<<endl;
+}
+}
