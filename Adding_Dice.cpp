1e9c295bd84070362b97fe89ce5d492d39e92ebe
AbinayaThapaChhetri <abhinayathapa164@gmail.com>
2026-07-27 20:03:40 +0545

    Add files via upload

---
+++ Adding_Dice.cpp
@@ -0,0 +1,29 @@
+  #include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void solve()
+{
+    ll x;
+    cin>>x;
+    if(x<3)
+    cout<<"No";
+else
+cout<<"Yes";
+    
+    
+    
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+   
+
+    
+   solve();
+    }
+
+    
+
